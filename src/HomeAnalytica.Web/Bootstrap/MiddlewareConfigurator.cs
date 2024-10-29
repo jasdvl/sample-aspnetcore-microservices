27da7e98f1d451c9a116c8e5d4f3caf247770f2b
jasdvl <166537903+jasdvl@users.noreply.github.com>
2024-10-29 01:27:03 +0100

    refactor(blazor): move DI service and middleware setup to CompositionRoot and MiddlewareConfigurator
    - Moved DI setup to CompositionRoot
    - Relocated middleware pipeline configuration (exception handling, HSTS, static files, Antiforgery) to MiddlewareConfigurator for improved structure and modularity

--- src/HomeAnalytica.Web/Bootstrap/MiddlewareConfigurator.cs
+++ src/HomeAnalytica.Web/Bootstrap/MiddlewareConfigurator.cs
@@ -1,6 +1,35 @@
+using HomeAnalytica.Web.Components;
+
 namespace HomeAnalytica.Web.Bootstrap
 {
+    /// <summary>
+    /// Configures the middleware components for the application.
+    /// </summary>
+    /// <remarks>
+    /// This class is responsible for setting up the middleware pipeline, including routing, 
+    /// authentication, authorization, logging, and other middleware components that handle 
+    /// incoming requests and outgoing responses. It should be called after the dependency 
+    /// injection configuration to ensure that all services are available for the middleware.
+    /// </remarks>
     public class MiddlewareConfigurator
     {
+        public static void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                // Default HSTS value is 30 days
+                // May be changed for production scenarios, see https://aka.ms/aspnetcore-hsts
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+        }
     }
 }
