04aeb12a48731491b35e76d982b1c30a40b6f82d
jasdvl <166537903+jasdvl@users.noreply.github.com>
2024-11-14 20:30:54 +0100

    chore: Adjust code style and cleanup

--- src/HomeAnalytica.Gateway.Yarp/Bootstrap/CompositionRoot.cs
+++ src/HomeAnalytica.Gateway.Yarp/Bootstrap/CompositionRoot.cs
@@ -1,48 +1,47 @@
-namespace HomeAnalytica.Gateway.Yarp.Bootstrap
+namespace HomeAnalytica.Gateway.Yarp.Bootstrap;
+
+/// <summary>
+/// The CompositionRoot class is responsible for setting up the application’s 
+/// dependency injection and service registration.
+/// </summary>
+/// <remarks>
+/// This class serves as the entry point for configuring the services that 
+/// will be used throughout the application. It creates a <see cref="WebApplicationBuilder"/> 
+/// instance and registers services, configuration settings, and environment 
+/// information necessary for the application to function correctly.
+/// </remarks>
+public class CompositionRoot
 {
     /// <summary>
-    /// The CompositionRoot class is responsible for setting up the application’s 
-    /// dependency injection and service registration.
+    /// Creates a <see cref="WebApplicationBuilder"/> instance with the specified 
+    /// <paramref name="options"/> and configures the application's services.
     /// </summary>
-    /// <remarks>
-    /// This class serves as the entry point for configuring the services that 
-    /// will be used throughout the application. It creates a <see cref="WebApplicationBuilder"/> 
-    /// instance and registers services, configuration settings, and environment 
-    /// information necessary for the application to function correctly.
-    /// </remarks>
-    public class CompositionRoot
+    /// <param name="options">The options to configure the web application.</param>
+    /// <returns>A configured <see cref="WebApplicationBuilder"/> instance.</returns>
+    public WebApplicationBuilder CreateBuilder(WebApplicationOptions options)
     {
-        /// <summary>
-        /// Creates a <see cref="WebApplicationBuilder"/> instance with the specified 
-        /// <paramref name="options"/> and configures the application's services.
-        /// </summary>
-        /// <param name="options">The options to configure the web application.</param>
-        /// <returns>A configured <see cref="WebApplicationBuilder"/> instance.</returns>
-        public WebApplicationBuilder CreateBuilder(WebApplicationOptions options)
+        var builder = WebApplication.CreateBuilder(options);
+
+        var environmentTarget = Environment.GetEnvironmentVariable("TARGET_ENVIRONMENT");
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+        if (!string.IsNullOrEmpty(environmentTarget))
         {
-            var builder = WebApplication.CreateBuilder(options);
-
-            var environmentTarget = Environment.GetEnvironmentVariable("TARGET_ENVIRONMENT");
-
-            builder.Configuration
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
-            if (!string.IsNullOrEmpty(environmentTarget))
-            {
-                builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.{environmentTarget}.json", optional: true, reloadOnChange: true);
-            }
-
-            // Allow non-secure HTTP for demo purposes only.
-            // Not recommended for production; use HTTPS in production environments.
-            builder.Services.AddAntiforgery(options => options.Cookie.SecurePolicy = CookieSecurePolicy.None);
-
-            builder.Services.AddReverseProxy()
-                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
-
-            return builder;
+            builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.{environmentTarget}.json", optional: true, reloadOnChange: true);
         }
+
+        // Allow non-secure HTTP for demo purposes only.
+        // Not recommended for production; use HTTPS in production environments.
+        builder.Services.AddAntiforgery(options => options.Cookie.SecurePolicy = CookieSecurePolicy.None);
+
+        builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+        return builder;
     }
 }
